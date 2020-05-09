fa8bccae4f353bfb3bc6b3b479033e665b993ffb
shun yamazaki <33094775+SyunYamazaki@users.noreply.github.com>
2020-05-09 15:25:15 +0900

    Initial Commit

---
+++ SimpleMusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMusicPlayer
+//
+//  Created by 山﨑駿 on 2020/05/09.
+//  Copyright © 2020 sYamaz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
